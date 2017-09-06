ec393c91c73b6d47d52b71006bed802b20a7dd9d
Clinton Dow <clint@datanavigator.ca>
2017-09-06 14:11:29 -0700

    Removed error handling for now

--- build.py
+++ build.py
@@ -5,17 +5,15 @@ import yaml
 
 DATA_PATH = os.path.join('.', '_data')
 HEADERS_FILE = 'headers.yaml'
-
-_errors = io.StringIO()
+YAML = '.yaml'
 
 try:
     _headers_stream = open(HEADERS_FILE)
-    _walk = [path for path in os.listdir(DATA_PATH) if path.endswith('.yaml')]
+    _walk = [path for path in os.listdir(DATA_PATH) if path.endswith(YAML)]
     _data_files = [os.path.join(DATA_PATH, path) for path in _walk]
     _dggs_data = [open(data) for data in _data_files]
-    _output_file = open('test.txt', 'w+')
-except IOError as _io_error:
-    _errors.write(str(_io_error))
+except IOError:
+    raise
 
 with _headers_stream:
     _headers = [h for h in yaml.load(_headers_stream, yaml.Loader)]
@@ -27,9 +25,3 @@ for _implementation in _dggs_data:
 
 _output = tabulate(tabular_data=list(_tabular_data), headers=_headers)
 print(_output)
-
-with _output_file:
-    _output_file.write(_output)
-
-if len(_errors.getvalue()):
-    print(_errors.getvalue())
