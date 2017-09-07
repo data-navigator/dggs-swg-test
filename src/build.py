13c868a990ffd677914f4dfd3ab57223b924b937
Clinton Dow <clint@datanavigator.ca>
2017-09-07 11:24:30 -0700

    Refactoring build.py to a proper module

--- src/build.py
+++ src/build.py
@@ -1,56 +1,72 @@
 # coding=utf-8
-"""Generate a HTML Page containing a DGGS Implementation Matrix to display via github-pages."""
+"""Creates a static HTML Page containing a DGGS Implementation Matrix to display
+via github-pages."""
 import os
 
 import markdown
 from tabulate import tabulate
 import yaml
 
-BODY = '<body>'
-BODY_CLOSE = '</body>'
-CSS = '<link rel="stylesheet" href="style.css">'
-DATA_DIR =  '_data'
-DOCS_DIR = 'docs'
-DOCTYPE = '<!DOCTYPE html>'
-FORMAT = 'pipe'
-JAVASCRIPT = '<script type="text/javascript" src="{}"></script>'
-JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
-SCRIPT = '<script src="{}"></script>'
-HEADERS_FILE = 'headers.yaml'
-HEAD = '<head>'
-HEAD_CLOSE = '</head>'
-HTML = '<html>'
-HTML_CLOSE = '</html>'
-INDEX_FILE = 'index.html'
-MD_TABLES_EXTENSION = 'markdown.extensions.tables'
-SCRIPT_FILE = 'script.js'
-TABLE_DIR = '_table'
-YAML = '.yaml'
+DIR_DATA = 'data'
+DIR_DOCS = 'docs'
+DIR_RES = 'res'
+FILE_FOOT = 'foot.html'
+FILE_HEAD = 'head.html'
+FILE_HEADERS = 'requirements.yaml'
+FILE_INDEX = 'index.html'
+FILE_SCRIPT = 'script.js'
+MD_EXTENSIONS = ['markdown.extensions.tables']
+TABLE_FORMAT = 'pipe'
 WRITE = 'w+'
+YAML = '.yaml'
 
-_pwd = os.path.dirname(os.path.realpath(__file__))
-_table_path = os.path.join(_pwd, TABLE_DIR)
-_headers_stream = open(os.path.join(_table_path, HEADERS_FILE))
-with _headers_stream:
-    _header_yaml = yaml.load(_headers_stream, yaml.Loader)
-    _headers = [header for header in _header_yaml]
-_data_path = os.path.join(_pwd, DATA_DIR)
-_files = [path for path in os.listdir(_data_path) if path.endswith(YAML)]
-_file_paths = [os.path.join(_data_path, path) for path in _files]
-_dggs_data_streams = [open(data) for data in _file_paths]
-_dggs_table = []
-for _dggs_data_stream in _dggs_data_streams:
-    with _dggs_data_stream:
-        _dggs_table.append(yaml.load(_dggs_data_stream, yaml.Loader))
-_jquery = SCRIPT.format(JQUERY_URL)
-_script = JAVASCRIPT.format(SCRIPT_FILE)
-_lead_tags = [DOCTYPE, HTML, HEAD, CSS, _jquery, _script, HEAD_CLOSE, BODY]
-_table = tabulate(_dggs_table, _headers, tablefmt=FORMAT)
-_body = markdown.markdown(_table, extensions=[MD_TABLES_EXTENSION])
-_end_tags = [BODY_CLOSE, HTML_CLOSE]
-_raw_html = '\n'.join(_lead_tags + [_body] + _end_tags)
-_docs_path = os.path.join(_pwd, DOCS_DIR)
-_output_file = os.path.join(_docs_path, INDEX_FILE)
-_output_stream = open(_output_file, WRITE)
-with _output_stream:
-    _output_stream.writelines(_raw_html)
+
+def generate_table_column_headers():
+    """Generate column headers for the DGGS Compliance Matrix table from the
+    .yaml files found in the ../res directory."""
+    res = os.path.join(os.path.pardir, DIR_RES)
+    header_stream = open(os.path.join(res, FILE_HEADERS))
+    with header_stream:
+        headers = yaml.load(header_stream, yaml.Loader)
+    yield from headers
+
+
+def generate_table_rows():
+    """Generate rows for the DGGS Compliance Matrix table from the .yaml files
+    found in the ../data directory."""
+    data = os.path.join(os.path.pardir, DIR_DATA)
+    filenames = [path for path in os.listdir(data) if path.endswith(YAML)]
+    file_paths = [os.path.join(data, filename) for filename in filenames]
+    data_streams = [open(data) for data in file_paths]
+    rows = []
+    for data_stream in data_streams:
+        with data_stream:
+            rows.append(yaml.load(data_stream, yaml.Loader))
+    yield from rows
+
+
+def create_table_html():
+    """Create the html table to embed in the body of index.html"""
+    rows = generate_table_rows()
+    cols = generate_table_column_headers()
+    table = tabulate(rows, cols, tablefmt=TABLE_FORMAT)
+    return markdown.markdown(table, extensions=MD_EXTENSIONS)
+
+
+def create_index_html():
+    """Create the html for the index file in the ../docs directory which will be
+    served on github-pages."""
+    _docs_path = os.path.join(_pwd, DOCS_DIR)
+    _output_file = os.path.join(_docs_path, INDEX_FILE)
+    _output_stream = open(_output_file, WRITE)
+    with _output_stream:
+        _output_stream.writelines(_raw_html)
+
+
+def main():
+    """Verifies all necessary files are present to build successfully. If not,
+    notify the user with an informative help message. If so write the index.html
+    files in ../docs."""
+
+if __name__ is '__main__':
+    main()
